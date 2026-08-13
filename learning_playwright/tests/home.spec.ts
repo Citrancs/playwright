48688b52c087860e61665f585d7da06621a1790b
Citrancs <h.marci1224@gmail.com>
2026-08-13 17:25:11 +0200

    Add authentication setup for customer 01 and update home page tests

--- learning_playwright/tests/home.spec.ts
+++ learning_playwright/tests/home.spec.ts
@@ -1,10 +1,10 @@
 import { test, expect } from "@playwright/test";
 
-test.describe("Home page tests", () => {
+test.describe("Home page with no authentication", () => {
   test.beforeEach(async ({ page }) => {
     await page.goto("https://practicesoftwaretesting.com/");
   });
-  
+
   test("Check sign in", async ({ page }) => {
     // Ensure the sign-in link is present on the home page
     await expect(page.getByTestId("nav-sign-in")).toHaveText("Sign in");
@@ -14,15 +14,15 @@ test.describe("Home page tests", () => {
     await expect(page).toHaveTitle(
       "Practice Software Testing - Toolshop - v5.0",
     );
-  })
-  
+  });
+
   test("Grid loads with 9 items", async ({ page }) => {
     // Check the count of items displayed
     const productGrid = page.locator(".col-md-9");
     await expect(productGrid.getByRole("link")).toHaveCount(9);
     expect(await productGrid.getByRole("link").count()).toBe(9);
-  })
-  
+  });
+
   test("Search for Thor Hammer", async ({ page }) => {
     // Search for Thor Hammer and check the result
     const productGrid = page.locator(".col-md-9");
@@ -30,6 +30,18 @@ test.describe("Home page tests", () => {
     await page.getByTestId("search-submit").click();
     await expect(productGrid.getByRole("link")).toHaveCount(1);
     await expect(page.getByAltText("Thor Hammer")).toBeVisible();
-  })
+  });
+});
+
+test.describe("Home page with customer 01 authentication", () => {
+  test.use({ storageState: ".auth/customer01.json" });
+  test.beforeEach(async ({ page }) => {
+    await page.goto("https://practicesoftwaretesting.com/");
+  });
   
+  test("Check customer 01 is signed in", async ({ page }) => {
+    // Ensure the customer 01 is signed in
+    await expect(page.getByTestId("nav-sign-in")).not.toBeVisible();
+    await expect(page.getByTestId("nav-menu")).toContainText("Jane Doe");
+  });
 });
